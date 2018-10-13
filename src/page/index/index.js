d30cd73a978fe4400ce0cbcbb2d21db7fcfb91af
kirito <1933590063@qq.com>
2018-10-13 23:07:59 +0800

    update ...

---
+++ src/page/index/index.js
@@ -0,0 +1,12 @@
+
+
+'use strict';
+
+
+var $ = require('jquery');
+$('body').html('HELLO INDEX');
+cats = require('./cats.js');
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var config = {
+	entry: './src/page/index/index.js',
+	output: {
+		path: './dist',
+		filename: 'app.js'
+	}
+};
+
+module.exports = config;
+
